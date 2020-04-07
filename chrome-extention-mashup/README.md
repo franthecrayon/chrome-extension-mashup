9e685294bf48d682180d2c7f1139d7cc2b800990
franthecrayon <60486554+franthecrayon@users.noreply.github.com>
2020-04-07 11:12:27 -0700

    final extension

---
+++ chrome-extention-mashup/README.md
@@ -0,0 +1,6 @@
+Daisies Everywhere
+==========
+
+This is the chrome extension that I created to grow flowers everywhere.
+ All the images on the webpage will be changed daisies.
+ and the background color of the text will be changed into violet.
---
+++ chrome-extention-mashup/content.js
@@ -0,0 +1,21 @@
+﻿var images = [
+  "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/daisy-flower-1532449822.jpg"
+];
+
+console.log("Load Extension");
+
+window.onload = replaceImages;
+
+function replaceImages() {
+    for (var i in document.images) {
+        var targetImage = document.images[i];
+        var randomImage = images[Math.floor(Math.random() * images.length)];
+        var height = targetImage.height;
+        var width = targetImage.width;
+
+        targetImage.src = randomImage;
+        targetImage.height = height;
+        targetImage.width = width;
+    }
+
+}
